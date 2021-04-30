5b5e41f60cd9a99eab570fd32b77543b2785ee3b
Oksana Svystun <svystuno.j@gmail.com>
2021-04-30 14:14:12 +0300

    Fixed adaptivity

--- script/script.js
+++ script/script.js
@@ -30,3 +30,10 @@ $('.carousel .carousel-item').each(function(){
         next.children(':first-child').clone().appendTo($(this));
       }
 });
+
+for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
+  e.style.setProperty('--value', e.value);
+  e.style.setProperty('--min', e.min == '' ? '0' : e.min);
+  e.style.setProperty('--max', e.max == '' ? '100' : e.max);
+  e.addEventListener('input', () => e.style.setProperty('--value', e.value));
+}
